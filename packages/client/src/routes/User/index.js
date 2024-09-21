6c4451a0c440fa5054c2654a233fe9354ff68cfa
FM9408 <medina.766@hotmail.com>
2024-09-20 20:32:07 -0600

    creacion del router del usuario

---
+++ packages/client/src/routes/User/index.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom"
+import { Homepage } from "../../pages/User/homepaje"
+import UserAppbar from "../../modules/Appbar"
+import { Box } from "@mui/material"
+
+
+function UserRouter() {
+    return (
+        <Box>
+            <UserAppbar />
+            <Routes>
+                <Route path='' element={<Homepage />} />
+            </Routes>
+        </Box>
+    )
+}
+
+
+export default UserRouter
---
+++ packages/client/src/routes/index.js
@@ -0,0 +1,10 @@
+import UserRouter from "./User/index"
+
+
+function MainRouter() {
+    return (
+        <UserRouter />
+    )
+} 
+
+export default MainRouter
